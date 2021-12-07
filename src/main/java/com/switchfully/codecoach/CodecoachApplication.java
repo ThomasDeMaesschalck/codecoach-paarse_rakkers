42d04f5d50ffb066dea42d7e2553e4e88f75af2a
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-07 16:10:56 +0100

    Initial commit

---
+++ src/main/java/com/switchfully/codecoach/CodecoachApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.codecoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/codecoach/CodecoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.codecoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
