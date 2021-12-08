eddda2c1222a80529245d1d828842225b290fe74
myrtheververs <myrthe.ververs@hotmail.com>
2021-12-08 13:11:39 +0100

    Changes by Klaas Devits

--- src/main/java/com/switchfully/codecoach/api/UserController.java
+++ src/main/java/com/switchfully/codecoach/api/UserController.java
@@ -1,5 +1,21 @@
 package com.switchfully.codecoach.api;
 
+import com.switchfully.codecoach.api.dto.UserDTO;
+import com.switchfully.codecoach.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+//@CrossOrigin
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
