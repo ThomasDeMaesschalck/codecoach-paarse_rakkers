3d10bb1330af7de68d57f7ab926d582f356053ff
myrtheververs <myrthe.ververs@hotmail.com>
2021-12-08 10:55:36 +0100

    user entity, database

--- src/main/java/com/switchfully/codecoach/repository/SessionRepository.java
+++ src/main/java/com/switchfully/codecoach/repository/SessionRepository.java
@@ -1,4 +1,14 @@
 package com.switchfully.codecoach.repository;
 
-public class SessionRepository {
+import com.switchfully.codecoach.domain.Session;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/*
+@Repository
+public interface SessionRepository extends JpaRepository<Session, UUID> {
 }
+*/
