1a3aa73c923924ba6aaca7c09df17b9949603cba
myrtheververs <myrthe.ververs@hotmail.com>
2021-12-08 13:11:33 +0100

    Changes by Andreeas Firoiu

--- src/main/java/com/switchfully/codecoach/api/dto/CoachInfoDTO.java
+++ src/main/java/com/switchfully/codecoach/api/dto/CoachInfoDTO.java
@@ -1,4 +1,9 @@
 package com.switchfully.codecoach.api.dto;
 
 public class CoachInfoDTO {
+
+    private UUID id;
+    private String introduction;
+    private String availability;
+    private List<TopicDTO> topics;
 }
