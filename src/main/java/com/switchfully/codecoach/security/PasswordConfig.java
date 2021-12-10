36013bdaa90f90d0b4c94a6073d5a0e892e0abf2
Andreeas Firoiu <andrfiro@gmail.com>
2021-12-10 13:40:43 +0100

    Changes by Klaas Devits

--- src/main/java/com/switchfully/codecoach/security/PasswordConfig.java
+++ src/main/java/com/switchfully/codecoach/security/PasswordConfig.java
@@ -3,7 +3,6 @@ package com.switchfully.codecoach.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.KeyPair;
@@ -15,7 +14,7 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return  NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
