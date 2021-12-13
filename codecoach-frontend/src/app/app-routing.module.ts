6c11ee40eff181f6061306ecb1913641f9c96576
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-13 10:20:06 +0100

    Added Register button in frontend.

--- codecoach-frontend/src/app/app-routing.module.ts
+++ codecoach-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {UserProfileComponent} from "./user/profile/user-profile/user-profile.component";
+import {RegisterComponent} from "./user/register/register/register.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'user/:id', component: UserProfileComponent}
+  {path: 'user/:id', component: UserProfileComponent},
+  {path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
