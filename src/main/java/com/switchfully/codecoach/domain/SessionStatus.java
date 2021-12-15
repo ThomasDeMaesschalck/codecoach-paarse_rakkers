b697a17ec505910b50ab04156f2b6a96aebc7674
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-15 08:47:48 +0100

    Added code that automatically adjusts session status to DONE_AWAITING_FEEDBACK or DONE after the session date is reached.

--- src/main/java/com/switchfully/codecoach/domain/SessionStatus.java
+++ src/main/java/com/switchfully/codecoach/domain/SessionStatus.java
@@ -4,6 +4,7 @@ public enum SessionStatus {
     REQUESTED,
     CANCELED_BY_COACH,
     CANCELED_BY_COACHEE,
+    DONE_WAITING_FEEDBACK,
     DONE,
     ACCEPTED
 }
--- src/main/java/com/switchfully/codecoach/repository/SessionRepository.java
+++ src/main/java/com/switchfully/codecoach/repository/SessionRepository.java
@@ -2,9 +2,11 @@ package com.switchfully.codecoach.repository;
 
 import com.switchfully.codecoach.domain.Session;
 
+import com.switchfully.codecoach.domain.SessionStatus;
 import com.switchfully.codecoach.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +15,7 @@ public interface SessionRepository extends JpaRepository<Session, UUID> {
     List<Session> findAllByCoach(User coach);
 
     int countAllByCoachAndCoachFeedbackNotNullAndCoacheeFeedbackNotNull(User coach);
+
+    List<Session> findAllByStatusNotAndMomentIsBefore(SessionStatus status, LocalDateTime moment);
+
 }
