b9c87f84fe5985ca0fddec5ad319d96fb84da52d
myrtheververs <myrthe.ververs@hotmail.com>
2021-12-16 16:08:54 +0100

    Changes by Klaas Devits

--- codecoach-frontend/src/app/session/sessions-overview/sessions-overview.component.ts
+++ codecoach-frontend/src/app/session/sessions-overview/sessions-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../authentication/authentication.service";
 import {SessionService} from "../session.service";
