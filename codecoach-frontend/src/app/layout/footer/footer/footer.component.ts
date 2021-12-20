0e0ac0ae3947dde1fbdf61cd2990aba8d3fdef4f
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-20 15:26:48 +0100

    Enhanced frontend styling.

--- codecoach-frontend/src/app/layout/footer/footer/footer.component.ts
+++ codecoach-frontend/src/app/layout/footer/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../../authentication/authentication.service";
 
 
 @Component({
@@ -8,7 +9,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authenticationService: AuthenticationService) { }
 
 
   ngOnInit(): void {
