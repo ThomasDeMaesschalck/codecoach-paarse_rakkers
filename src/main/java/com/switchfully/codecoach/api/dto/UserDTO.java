75c920794497bc55dd17a0167f127f283726abe7
Klaas Devits <klaas.devits@gmail.com>
2021-12-22 15:05:53 +0100

    email validation regexp added

--- src/main/java/com/switchfully/codecoach/api/dto/UserDTO.java
+++ src/main/java/com/switchfully/codecoach/api/dto/UserDTO.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 @Data
 public class UserDTO {
 
-
     private UUID id;
     @NotBlank(message = "First name cannot be blank")
     @NotNull(message = "First name cannot be null")
@@ -24,7 +23,7 @@ public class UserDTO {
     @NotBlank(message = "Last name cannot be blank")
     @NotNull(message = "Last name cannot be null")
     private String lastName;
-    @Email(message = "Email has to be valid")
+    @Email(message = "Email has to be valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
     @NotBlank(message = "Password cannot be blank")
     @NotNull(message = "Password cannot be null")
